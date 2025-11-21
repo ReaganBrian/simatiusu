15a5e70a720642cef9c2e1d3632727bebd804d55
ReaganBrian <reagansiahaan2006@gmail.com>
2025-11-21 17:21:37 +0700

    Tes commit

--- src/main/java/com/mycompany/simatiusu/Simatiusu.java
+++ src/main/java/com/mycompany/simatiusu/Simatiusu.java
@@ -11,6 +11,6 @@ package com.mycompany.simatiusu;
 public class Simatiusu {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Simati usu");
     }
 }
